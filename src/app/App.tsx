5128acd6b4911bbb5ae822be4c9d7786168ec1b7
Artem <cspirogone@yandex.ru>
2026-01-09 22:31:56 +0300

    fix themeToggler

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,8 +8,6 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import AppBar from './AppBar.tsx'
-import { ThemeProvider } from '@mui/material'
-import theme from './Themes.tsx'
 
 function App() {
 	const [count, setCount] = useState(0)
@@ -17,30 +15,28 @@ function App() {
 
 	return (
 		<>
-			<ThemeProvider defaultMode="light" theme={theme}>
-				<AppBar />
-				<div>
-					<a href="https://vite.dev" target="_blank">
-						<img src={viteLogo} className="logo" alt="Vite logo" />
-					</a>
-					<a href="https://react.dev" target="_blank">
-						<img src={reactLogo} className="logo react" alt="React logo" />
-					</a>
-				</div>
-				<input value={text} onChange={(e) => setText(e.target.value)} />
-				<p>Typed text:{text}</p>
-				<h1>Vite + React</h1>
-				<div className="card">
-					<Button variant={'contained'} onClick={() => setCount((count) => count + 1)}>
-						count is {count}
-					</Button>
-					<p>
-						Edit <code>src/App.tsx</code> and save to test HMR
-					</p>
-				</div>
-				<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-				<Button variant="contained">Hello world</Button>
-			</ThemeProvider>
+			<AppBar />
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<input value={text} onChange={(e) => setText(e.target.value)} />
+			<p>Typed text:{text}</p>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<Button variant={'contained'} onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</Button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			<Button variant="contained">Hello world</Button>
 		</>
 	)
 }
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,10 +2,39 @@ import { createRoot } from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 import App from './App.tsx'
+import { createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+	colorSchemes: {
+		light: {
+			palette: {
+				primary: {
+					main: '#1976d2',
+				},
+				secondary: {
+					main: '#080707',
+				},
+			},
+		},
+		dark: {
+			palette: {
+				primary: {
+					main: '#90caf9',
+				},
+				secondary: {
+					main: '#42a5f5',
+				},
+			},
+		},
+	},
+})
 
 createRoot(document.getElementById('root')!).render(
 	<>
-		<CssBaseline />
-		<App />
+		<ThemeProvider defaultMode="light" theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</>
 )
