ece63a1bd53d0d6443e2d67416a765524b579b16
Artem <cspirogone@yandex.ru>
2026-01-19 11:59:09 +0300

    feat Authorization and registration functions have been implemented

--- src/app/AppBar.tsx
+++ src/app/AppBar.tsx
@@ -8,7 +8,13 @@ import FormControlLabel from '@mui/material/FormControlLabel'
 import FlareIcon from '@mui/icons-material/Flare'
 import Brightness4Icon from '@mui/icons-material/Brightness4'
 
-const ButtonAppBar = () => {
+type Props = {
+	access_token?: string
+	username?: string
+}
+
+const ButtonAppBar = (props: Props) => {
+	const { username } = props
 	const { mode, setMode } = useColorScheme()
 	if (!mode) {
 		return null
@@ -17,6 +23,28 @@ const ButtonAppBar = () => {
 	const handleToggleTheme = (_event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
 		setMode(checked ? 'dark' : 'light')
 	}
+
+	const stringToColor = (string: string) => {
+		let hash = 0
+		let i
+
+		for (i = 0; i < string.length; i += 1) {
+			hash = string.charCodeAt(i) + ((hash << 5) - hash)
+		}
+		let color = '#'
+		for (i = 0; i < 3; i += 1) {
+			const value = (hash >> (i * 8)) & 0xff
+			color += `00${value.toString(16)}`.slice(-2)
+		}
+		return color
+	}
+
+	const stringAvatar = (name: string) => ({
+		sx: {
+			bgcolor: stringToColor(name),
+		},
+		children: `${name.split(' ')[0][0]}${name.split(' ')[1]?.[0] || ''}`.toUpperCase(),
+	})
 
 	const IOSSwitch = styled((props: SwitchProps) => (
 		<Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
@@ -86,9 +114,11 @@ const ButtonAppBar = () => {
 						<MenuIcon />
 					</IconButton>
 					<Stack direction={'row'} spacing={2} style={{ flexGrow: 1 }}>
-						<Typography variant="h6" component="div">
-							Todos
-						</Typography>
+						{username && (
+							<Typography variant="h6" component="div">
+								Todos
+							</Typography>
+						)}
 						<Typography variant="h6" component="div">
 							About
 						</Typography>
@@ -101,12 +131,15 @@ const ButtonAppBar = () => {
 						onChange={handleToggleTheme}
 					/>
 					<Stack direction="row">
-						<Brightness4Icon />
+						<Brightness4Icon style={{ marginRight: '10px' }} />
 					</Stack>
-					<Button color="inherit">Login</Button>
-					<Tooltip title="User">
-						<Avatar src={''} />
-					</Tooltip>
+					{username ? (
+						<Tooltip title="User">
+							<Avatar src={''} alt={username} {...stringAvatar(username)} />
+						</Tooltip>
+					) : (
+						<Button color="inherit">Login</Button>
+					)}
 				</Toolbar>
 			</AppBar>
 		</Box>
