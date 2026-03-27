80fa086d694154e9772e23207678ca6bb40b6b00
Artem <cspirogone@yandex.ru>
2026-03-27 14:03:46 +0300

    валидация форм(авторизация/регистрация) с помощью zod и react-hook-form

--- src/entities/User/ui/Auth.tsx
+++ src/entities/User/ui/Auth.tsx
@@ -1,4 +1,3 @@
-import React, { type SyntheticEvent, useState } from 'react'
 import {
 	Button,
 	Container,
@@ -14,32 +13,61 @@ import { handleLogin, handleRegister } from '../api/userApi.ts'
 import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
 import { selectLoading, selectUser } from '../model/store/userStore.ts'
 import { Navigate, useLocation, useNavigate, useParams } from 'react-router'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type SignUpFormInputs = z.infer<typeof stringSchemaRegister>
+type SignInFormInputs = z.infer<typeof stringSchemaLogin>
+const stringSchemaRegister = z
+	.object({
+		userName: z.string().max(10, { message: 'Name so long. Max length 10 symbols' }),
+		userPassword: z.string().min(3, { message: 'Password must be at least 3 characters' }),
+		confermedPassword: z.string(), //схема валидаций
+	})
+	.refine((data) => data.userPassword === data.confermedPassword, {
+		message: "Passwords don't match",
+		path: ['confermedPassword'],
+	})
+
+const stringSchemaLogin = z.object({
+	userName: z.string().max(10, { message: 'Name so long. Max length 10 symbols' }),
+	userPassword: z.string().min(5, { message: 'Password must be at least 5 characters' }),
+})
 
 const Auth = () => {
-	const [userName, setUserName] = useState('')
-	const [userPassword, setUserPassword] = useState('')
 	const dispatch = useAppDispatch()
 	const loading = useAppSelector(selectLoading)
 	const navigate = useNavigate()
 	const user = useAppSelector(selectUser)
 	const { mode } = useParams()
 	const stateLocation = useLocation().state
+	//тут использую zod c react-hook-form
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm<SignUpFormInputs>({
+		resolver: zodResolver(stringSchemaRegister),
+		mode: 'onChange',
+	})
+	// стейт для хранения логинизации
+	const {
+		register: login,
+		handleSubmit: handleSubmitLogin,
+		formState: { errors: loginErrors },
+	} = useForm<SignInFormInputs>({
+		resolver: zodResolver(stringSchemaLogin),
+		mode: 'onChange',
+	})
 
-	if(user) {
-		return <Navigate to={stateLocation || '/'}/>
-	}
-
-
-	const handleUserNameChange = (e: SyntheticEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-		setUserName(e.currentTarget.value)
-	}
-	const handleUserPasswordChange = (e: SyntheticEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-		setUserPassword(e.currentTarget.value)
+	if (user) {
+		return <Navigate to={stateLocation || '/'} />
 	}
 
 	const handleResetFields = () => {
-		setUserName('')
-		setUserPassword('')
+		reset()
 	}
 
 	const handleChange = (_event: React.MouseEvent, mode: string) => {
@@ -66,105 +94,141 @@ const Auth = () => {
 					<ToggleButton value="register">Register</ToggleButton>
 				</ToggleButtonGroup>
 				{mode === 'login' ? (
-					<Stack direction={'column'} spacing={2}>
-						<TextField
-							disabled={loading ? true : false}
-							id="filled-email-input"
-							label="email"
-							variant="filled"
-							value={userName}
-							onChange={handleUserNameChange}
-							slotProps={{
-								input: {
-									startAdornment: (
-										<InputAdornment position="start">
-											<AccountCircle />
-										</InputAdornment>
-									),
-								},
-							}}
-						/>
-						<TextField
-							disabled={loading ? true : false}
-							id="filled-password-input"
-							label="password"
-							type="password"
-							variant="filled"
-							value={userPassword}
-							onChange={handleUserPasswordChange}
-							slotProps={{
-								input: {
-									startAdornment: (
-										<InputAdornment position="start">
-											<AccountCircle />
-										</InputAdornment>
-									),
-								},
-							}}
-						/>
-						<Button
-							variant="contained"
-							color="primary"
-							onClick={() => handleLogin(navigate, dispatch, userName, userPassword)}
-							fullWidth
-							loadingPosition="start"
-							loading={loading}
-						>
-							{loading ? 'Loading' : 'LOGIN'}
-						</Button>
-					</Stack>
+					<form
+						onSubmit={handleSubmitLogin((data) => {
+							handleLogin(navigate, dispatch, data.userName, data.userPassword)
+						})}
+					>
+						<Stack direction={'column'} spacing={2}>
+							<TextField
+								disabled={loading ? true : false}
+								id="filled-email-input"
+								label="name"
+								variant="filled"
+								{...login('userName')}
+								error={!!loginErrors?.userName}
+								helperText={loginErrors ? loginErrors.userName?.message : ''}
+								slotProps={{
+									input: {
+										startAdornment: (
+											<InputAdornment position="start">
+												<AccountCircle />
+											</InputAdornment>
+										),
+									},
+								}}
+							/>
+							<TextField
+								disabled={loading ? true : false}
+								id="filled-password-input"
+								label="password"
+								type="password"
+								variant="filled"
+								{...login('userPassword')}
+								error={!!loginErrors?.userPassword}
+								helperText={loginErrors ? loginErrors.userPassword?.message : ''}
+								slotProps={{
+									input: {
+										startAdornment: (
+											<InputAdornment position="start">
+												<AccountCircle />
+											</InputAdornment>
+										),
+									},
+								}}
+							/>
+							<Button
+								type="submit"
+								variant="contained"
+								color="primary"
+								fullWidth
+								loadingPosition="start"
+								loading={loading}
+							>
+								{loading ? 'Loading' : 'LOGIN'}
+							</Button>
+						</Stack>
+					</form>
 				) : (
-					<Stack direction={'column'} spacing={2}>
-						<TextField
-							disabled={loading ? true : false}
-							id="filled-email-input"
-							label="email"
-							variant="filled"
-							value={userName}
-							onChange={handleUserNameChange}
-							slotProps={{
-								input: {
-									startAdornment: (
-										<InputAdornment position="start">
-											<AccountCircle />
-										</InputAdornment>
-									),
-								},
-							}}
-						/>
-						<TextField
-							disabled={loading ? true : false}
-							id="filled-password-input"
-							label="password"
-							type="password"
-							variant="filled"
-							value={userPassword}
-							onChange={handleUserPasswordChange}
-							slotProps={{
-								input: {
-									startAdornment: (
-										<InputAdornment position="start">
-											<AccountCircle />
-										</InputAdornment>
-									),
-								},
-							}}
-						/>
-						<Button variant="contained" color="primary" fullWidth onClick={handleResetFields}>
-							Reset fields
-						</Button>
-						<Button
-							variant="contained"
-							color="primary"
-							onClick={() => handleRegister(navigate, dispatch, userName, userPassword)}
-							fullWidth
-							loadingPosition="start"
-							loading={loading}
-							sx={{ backgroundColor: mode === 'login' ? '#1976d2' : '#dc004e' }}
-						>
-							{loading ? 'Loading' : 'Register'}
-						</Button>
-					</Stack>
+					<form
+						onSubmit={handleSubmit((data) => {
+							handleRegister(navigate, dispatch, data.userName, data.userPassword)
+						})}
+					>
+						<Stack direction={'column'} spacing={2}>
+							<TextField
+								disabled={loading ? true : false}
+								id="filled-email-input"
+								label="name"
+								variant="filled"
+								{...register('userName')}
+								error={!!errors?.userName}
+								helperText={errors ? errors.userName?.message : ''}
+								// onChange={handleUserNameChange}
+								slotProps={{
+									input: {
+										startAdornment: (
+											<InputAdornment position="start">
+												<AccountCircle />
+											</InputAdornment>
+										),
+									},
+								}}
+							/>
+							<TextField
+								disabled={loading ? true : false}
+								id="filled-password-input"
+								label="password"
+								type="password"
+								variant="filled"
+								{...register('userPassword')}
+								error={!!errors?.userPassword}
+								helperText={errors ? errors.userPassword?.message : ''} //удобный пропс коменонента от mui
+								slotProps={{
+									input: {
+										startAdornment: (
+											<InputAdornment position="start">
+												<AccountCircle />
+											</InputAdornment>
+										),
+									},
+								}}
+							/>
+							<TextField
+								disabled={loading ? true : false}
+								id="filled-password-input"
+								label="Confermed password"
+								type="password"
+								variant="filled"
+								{...register('confermedPassword')}
+								error={!!errors?.confermedPassword}
+								helperText={errors ? errors.confermedPassword?.message : ''} //удобный пропс коменонента от mui
+								slotProps={{
+									input: {
+										startAdornment: (
+											<InputAdornment position="start">
+												<AccountCircle />
+											</InputAdornment>
+										),
+									},
+								}}
+							/>
+							<Button variant="contained" color="primary" fullWidth onClick={handleResetFields}>
+								Reset fields
+							</Button>
+							<Button
+								type="submit"
+								variant="contained"
+								color="primary"
+								fullWidth
+								loadingPosition="start"
+								loading={loading}
+								sx={{ backgroundColor: mode === 'login' ? '#1976d2' : '#dc004e' }}
+							>
+								{loading ? 'Loading' : 'Register'}
+							</Button>
+						</Stack>
+					</form>
 				)}
 			</Paper>
 		</Container>
